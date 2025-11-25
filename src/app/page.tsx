af7bf3bfe55715975090dea74e628f6fa3c6a702
Tahmid Azam <tahmid_azam@icloud.com>
2025-11-25 16:05:06 +0000

    fix: remove unused vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
-"use client";
-
+import { ButtonArray } from "@/components/button-array";
 import { FixBottom } from "@/components/fix-bottom";
-import { InstallPrompt } from "@/components/install-prompt";
 import { FixTop } from "@/components/fix-top";
+import { InstallPrompt } from "@/components/install-prompt";
+import { NavigationBar } from "@/components/navigation-bar";
 import { NewCheckButton } from "@/components/new-check-button";
 import { ResultsTable } from "@/components/results-table";
-import { useAppStore } from "@/providers/app-store-provider";
-import { useRouter } from "next/navigation";
 import { SafeAreaContentWrapper } from "@/components/safe-area-content-wrapper";
-import { NavigationBar } from "@/components/navigation-bar";
-import { ButtonArray } from "@/components/button-array";
 
 export default function Home() {
-  const results = useAppStore((state) => state.results);
-  const router = useRouter();
-
   return (
     <>
       <FixTop>
