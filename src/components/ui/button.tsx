71c6d71afe421a5105ec5ff83dfe75c9b76bb88f
Tahmid Azam <tahmid_azam@icloud.com>
2025-11-25 11:11:48 +0000

    fix: button select behaviours

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -52,7 +52,10 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(
+        buttonVariants({ variant, size, className }),
+        "select-none"
+      )}
       {...props}
     />
   );
